e31559c185b780191007fab0148e816efb22154a
PvBlackSins <paulovictor.pv05@gmail.com>
2024-12-18 16:40:52 -0300

    commit add passo 3

--- determination.py
+++ determination.py
@@ -1,158 +1,153 @@
+import os
 import re
 
 def parse_automaton(file_path):
-  # open and close file after scope
-  with open(file_path, 'r') as file:
-    # transform each line in array
-    lines = file.read().splitlines()
+    with open(file_path, 'r') as file:
+        lines = file.read().splitlines()
 
-  automaton = {}
-  # creates an array to state and symbols values
-  automaton['states'] = re.findall(r'\b\w+\b', lines[0].split('=')[1])
-  automaton['symbols'] = re.findall(r'\b\w+\b', lines[1].split('=')[1])
+    automaton = {}
+    automaton['states'] = re.findall(r'\b\w+\b', lines[0].split('=')[1])
+    automaton['symbols'] = re.findall(r'\b\w+\b', lines[1].split('=')[1])
 
-  transitions = {}
-  for line in lines[2:]:
-    #   continue
-    # verify if each transition is in (qx, 0) -> qy pattern
-    match = re.match(r'\((\w+), (\w+)\) -> (\w+)', line.strip())
-    if match:
-      # separate each state and symbol
-      current, symbol, next_state = match.groups()
-      # add transition to transitions dict
-      transitions.setdefault((current, symbol), []).append(next_state)
-  automaton['transitions'] = transitions
+    transitions = {}
+    for line in lines[2:]:
+        if line.startswith("q") or line.startswith("F") or line.startswith("w"):
+            break
+        match = re.match(r'\((\w+), (\w+)\) -> (\w+)', line.strip())
+        if match:
+            current, symbol, next_state = match.groups()
+            transitions.setdefault((current, symbol), []).append(next_state)
+    automaton['transitions'] = transitions
 
-  # add start_state to automaton
-  automaton['start_state'] = lines[-2].split('=')[1].strip()
-  # add final_states to automaton
-  automaton['final_states'] = re.findall(r'\b\w+\b', lines[-1].split('=')[1])
+    initial_line = next(line for line in lines if ": inicial" in line)
+    automaton['start_state'] = initial_line.split(":")[0].strip()
 
-  return automaton
+    final_line = next(line for line in lines if line.startswith("F:"))
+    automaton['final_states'] = re.findall(r'\b\w+\b', final_line.split(":")[1])
+
+    word_line = next(line for line in lines if line.startswith("w:"))
+    automaton['input_string'] = word_line.split(":")[1].strip()
+
+    return automaton
 
 def build_e_closure(automaton):
-  e_closures= {}
-  for state in automaton['states']:
-    # get state returned by void transitions
-    e_closure_states = automaton['transitions'].get((state, 'vazio'), [])
-    # add self state to e_closure
-    e_closures[state] = [state]
-    if len(e_closure_states) != 0:
-      # add each state returned by void transitions to e_closure
-      e_closures[state].extend(e_closure_states)
-  return e_closures
+    e_closures = {}
+    for state in automaton['states']:
+        e_closure_states = automaton['transitions'].get((state, 'vazio'), [])
+        e_closures[state] = [state] + e_closure_states
+    return e_closures
 
 def build_afd(automaton, e_closures):
-  start_state = tuple(e_closures[automaton['start_state']])  # Estado inicial do AFD
-  afd_states = {start_state}  # AFD state
-  afd_transitions = {}  # AFD transitions
-  unprocessed_states = [start_state]  # pending process states
-  afd_final_states = []  # final AFD states
+    start_state = tuple(e_closures[automaton['start_state']])
+    afd_states = {start_state}
+    afd_transitions = {}
+    unprocessed_states = [start_state]
+    afd_final_states = []
 
-  # process afd states
-  while unprocessed_states:
-    current_state = unprocessed_states.pop(0)  # current unprocessed state
-    
-    for symbol in automaton['symbols']:
-      # find and save reachable states by symbol
-      reachable = set()
-      for sub_state in current_state:
-        reachable.update(automaton['transitions'].get((sub_state, symbol), []))
+    while unprocessed_states:
+        current_state = unprocessed_states.pop(0)
+        for symbol in automaton['symbols']:
+            reachable = set()
+            for sub_state in current_state:
+                reachable.update(automaton['transitions'].get((sub_state, symbol), []))
+            e_closure_reachable = set()
+            for state in reachable:
+                e_closure_reachable.update(e_closures[state])
+            e_closure_tuple = tuple(sorted(e_closure_reachable))
+            if e_closure_tuple not in afd_states:
+                afd_states.add(e_closure_tuple)
+                unprocessed_states.append(e_closure_tuple)
+            afd_transitions[(current_state, symbol)] = e_closure_tuple
 
-      # calculate e-closure of reachable state
-      e_closure_reachable = set()
-      for state in reachable:
-        e_closure_reachable.update(e_closures[state])
+    for afd_state in afd_states:
+        if any(state in automaton['final_states'] for state in afd_state):
+            afd_final_states.append(afd_state)
 
-      e_closure_tuple = tuple(sorted(e_closure_reachable))
-
-      # register a new state if state is not inside afd_states and add new state to unprocessed_states to be processed on next loop
-      if e_closure_tuple not in afd_states:
-        afd_states.add(e_closure_tuple)
-        unprocessed_states.append(e_closure_tuple)
-
-      # register transition on AFD
-      afd_transitions[(current_state, symbol)] = e_closure_tuple
-
-  # identify final states on AFD
-  for afd_state in afd_states:
-    # Is a final state if final AFN state is in any of AFD state 
-    if any(state in automaton['final_states'] for state in afd_state):
-      afd_final_states.append(afd_state)
-  
-  # return AFD
-  return {
-    'states': list(afd_states),
-    'symbols': automaton['symbols'] ,
-    'transitions': afd_transitions,
-    'start_state': start_state,
-    'final_states': afd_final_states,
-  }
+    return {
+        'states': list(afd_states),
+        'symbols': automaton['symbols'],
+        'transitions': afd_transitions,
+        'start_state': start_state,
+        'final_states': afd_final_states,
+    }
 
 def build_afd_complement(afd):
-  afd_final_states = []
-  for state in afd['states']:
-    if state not in afd['final_states']:
-      afd_final_states.append(state)
-  
-  return {
-    'states': afd['states'],
-    'symbols': afd['symbols'],
-    'transitions': afd['transitions'],
-    'start_state': afd['start_state'],
-    'final_states': afd_final_states,
-  }
+    afd_final_states = [state for state in afd['states'] if state not in afd['final_states']]
+    return {
+        'states': afd['states'],
+        'symbols': afd['symbols'],
+        'transitions': afd['transitions'],
+        'start_state': afd['start_state'],
+        'final_states': afd_final_states,
+    }
 
-def create_afd_with_one_final_state(afd):
-  new_final_state = tuple(['qf'])
-  states = set(afd['states'])
-  states.add(new_final_state)
+def build_reverse_afd(afd):
+    reverse_transitions = {}
+    for (state, symbol), next_state in afd['transitions'].items():
+        reverse_transitions.setdefault((next_state, symbol), []).append(state)
 
-  afd_transitions = {}
-  afd_transitions.update(afd['transitions'])
+    reverse_final_states = [afd['start_state']]
+    reverse_start_state = tuple(afd['final_states'])
 
-  for final_state in afd['final_states']:
-    afd_transitions[(final_state, 'vazio')] = 'qf'
+    return {
+        'states': afd['states'],
+        'symbols': afd['symbols'],
+        'transitions': reverse_transitions,
+        'start_state': reverse_start_state,
+        'final_states': reverse_final_states,
+    }
 
-  return {
-    'states': list(states),
-    'symbols': afd['symbols'],
-    'transitions': afd_transitions,
-    'start_state': afd['start_state'],
-    'final_states': new_final_state,
-  }
+def simulate_afd(afd, input_string):
+    current_state = afd['start_state']
+    for symbol in input_string:
+        transition_key = (current_state, symbol)
+        if transition_key in afd['transitions']:
+            current_state = afd['transitions'][transition_key]
+        else:
+            return False
+    return current_state in afd['final_states']
 
-def build_reverse_afd(af):
-  if len(af['final_states']) > 1:
-    afn_with_one_final_state = create_afd_with_one_final_state(afd)
-    # print('afn_with_one_final_state', afn_with_one_final_state)
-  if afn_with_one_final_state:
-    af = afn_with_one_final_state
+def save_automaton_to_file(automaton, file_name, include_input_string=True):
+    try:
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        full_file_path = os.path.join(dir_path, file_name)
 
-  print('afn_with_one_final_state', afn_with_one_final_state)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
-  af_transitions = {}
-  for (state, symbol), next_state in af['transitions'].items():
-    # af_transitions[(next_state, symbol)] = state
-    af_transitions.setdefault((next_state, symbol), []).append(state)
+        with open(full_file_path, 'w', encoding='utf-8') as file:
+            file.write(f"states = {', '.join(str(state) for state in automaton['states'])}\n")
+            file.write(f"symbols = {', '.join(str(symbol) for symbol in automaton['symbols'])}\n")
+            
+            for (state, symbol), next_state in automaton['transitions'].items():
+                if symbol == 'vazio':
+                    file.write(f"({state}, vazio) -> {', '.join(str(s) for s in next_state)}\n")
+                else:
+                    file.write(f"({state}, {symbol}) -> {', '.join(str(s) for s in next_state)}\n")
+            
+            file.write(f"{automaton['start_state']}: inicial\n")
+            file.write(f"F: {', '.join(str(state) for state in automaton['final_states'])}\n")
+            
+            if include_input_string:
+                file.write(f"w: {automaton['input_string']}\n")
+            
+        print(f"Arquivo salvo: {full_file_path}")
+    
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo {file_name}: {e}")
 
-  return {
-    'states': af['states'],
-    'symbols': af['symbols'],
-    'transitions': af_transitions,
-    'start_state': af['final_states'],
-    'final_states': af['start_state'],
-  }
-  
-
-
-
-automaton = parse_automaton('afn.txt')
+input_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "afn.txt")
+automaton = parse_automaton(input_file)
 e_closures = build_e_closure(automaton)
 afd = build_afd(automaton, e_closures)
 afd_complement = build_afd_complement(afd)
 reverse_afd = build_reverse_afd(afd)
-# print('afd', afd)
-print('reverse_afd', reverse_afd)
-# print(afd)
 
+save_automaton_to_file(automaton, "AFN Original.txt", include_input_string=True)
+save_automaton_to_file(afd, "AFD.txt", include_input_string=False)
+save_automaton_to_file(afd_complement, "COMP.txt", include_input_string=False)
+save_automaton_to_file(reverse_afd, "REV.txt", include_input_string=False)
+
+input_string = automaton['input_string']
+is_accepted = simulate_afd(afd, input_string)
+print(f"A cadeia '{input_string}' foi {'ACEITA' if is_accepted else 'REJEITADA'} pelo AFD.")
